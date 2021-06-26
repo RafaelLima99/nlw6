0ac8b40de860fa77a499f0b41796523be8af1f23
RafaelLimaUnu <rafael.lima@unu.com.br>
2021-06-26 10:35:21 -0300

    Aula 3

--- src/pages/NewRoom.tsx
+++ src/pages/NewRoom.tsx
@@ -1,14 +1,41 @@
+import { useState } from 'react';
+import { FormEvent } from 'react';
 import { Link } from 'react-router-dom';
 import ilustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
+import { database } from '../services/firebase';
 import '../styles/auth.scss'
-// import { useAuth } from '../hooks/useAuth';
+import { useAuth } from '../hooks/useAuth';
 
 // toda função que começa com a palavra "use" é um hook
 export function NewRoom(){
 
-    // const { user } = useAuth;
+    const { user } = useAuth();
+
+    const [newRoom, setNewRoom] = useState('');
+
+    // o tipo FormEvent vem de um import chamdo FormEvent
+    async function handleCreateRoom(event: FormEvent) {
+        // preventDefaul retirar a função padrão do evento 
+        // que no caso do evento de um formulário é redirecionar para outra pagina ou recarregar a pagina
+        // nesse caso o preventDefault impede que isso aconteça
+        event.preventDefault();
+
+        //trim remove os espaços de uma string
+        if(newRoom.trim() === ''){
+            return;
+        }
+        //ref ou referencia é como se fosse uma tabela no banco de dados do firebase
+        const roomRef = database.ref('rooms');
+        const firebaseRoom = await roomRef.push({
+            title: newRoom,
+            authorId: user?.id 
+        })
+
+
+        console.log(newRoom)
+    }
 
     return(
         <div id="page-auth">
@@ -24,9 +51,11 @@ export function NewRoom(){
                     ou seja só exibe o user.name se ele não for undefine, só exibe se user.name tiver algum dado*/}
                     {/* <h1>{user?.name}</h1> */}
                     <h2>Criar uma nova sala</h2>
-                    <form>
+                    <form onSubmit={handleCreateRoom}>
                         <input type="text"
                         placeholder="Nome da sala"
+                        onChange={event => setNewRoom(event.target.value)}
+                        value={newRoom}
                         />
                         <Button type="submit">
                             Criar sala
