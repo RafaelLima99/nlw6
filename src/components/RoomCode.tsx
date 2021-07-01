605c9400410b7a6162daf69af7757c5141ef735c
RafaelLimaUnu <rafael.lima@unu.com.br>
2021-07-01 12:10:14 -0300

    Aula-3

--- src/components/RoomCode.tsx
+++ src/components/RoomCode.tsx
@@ -1,17 +1,24 @@
 import copyImg from "../assets/images/copy.svg";
 import "../styles/room-code.scss";
-export function RoomCode() {
 
-    function copyRoomCode (){
 
+type RoomCodeProps = {
+    code: string;
+} 
+
+export function RoomCode(props: RoomCodeProps) {
+
+    function copyRoomCodeToClipboard (){
+        navigator.clipboard.writeText(props.code)
+        alert("Copiado com sucesso!")
     }
 
     return(
-        <button className="room-code">
+        <button className="room-code" onClick={copyRoomCodeToClipboard}>
             <div>
                 <img src={copyImg} alt="Copy room code" />
             </div>
-            <span>Sala #1238123712321</span>
+            <span>Sala #{props.code}</span>
         </button>
     )
 }
--- src/pages/Room.tsx
+++ src/pages/Room.tsx
@@ -1,14 +1,61 @@
 
+import { FormEvent, useState } from "react";
+import { useParams } from "react-router-dom";
 import logoImg from "../assets/images/logo.svg"
 import { Button } from "../components/Button";
+import { RoomCode } from "../components/RoomCode";
+import { useAuth } from "../hooks/useAuth";
+import { database } from "../services/firebase";
 import "../styles/room.scss";
+
+
+type RoomParams = {
+    id : string;
+}
+
 export function Room() {
+
+    //pega os parametros da url
+    //é do tipo RoomParams
+    const params = useParams<RoomParams>();
+     
+    const [newQuestion, setNewQuestion] = useState('')
+
+    const roomId = params.id;
+
+    const {user} = useAuth();
+
+    async function handlerSendQuestion(event: FormEvent){
+        event.preventDefault();
+        if(newQuestion.trim() == ""){
+            return;
+        }
+
+        if(!user){
+            throw new Error('You must be logged in');
+        }
+
+        const question = {
+            content: newQuestion,
+            author: {
+                name:user.name,
+                avatar: user.avatar,
+            },
+            isHighLighted: false,
+            isAnswered: false
+        };
+
+        //adciona uma question na room atual
+        //seleciona a room que o usuario esta pelo id e adciona uma question nela
+        await database.ref(`rooms/${roomId}/questions`).push(question);
+    }
+
     return(
         <div id="page-room">
             <header>
                 <div className="content">
                     <img src={logoImg} alt="Letmeask" />
-                    <div>codigo</div>
+                    <RoomCode code={roomId} />
                 </div>
             </header>
             <div className="main">
@@ -16,12 +63,13 @@ export function Room() {
                     <h1>Sala React</h1>
                     <span>4 perguntas</span>
                 </div>
-                <form>
-                    <textarea placeholder="O que você quer perguntar?" />
-                    
+                <form onSubmit={handlerSendQuestion}>
+                    <textarea placeholder="O que você quer perguntar?"
+                    onChange={event => setNewQuestion(event.target.value)} 
+                    value={newQuestion}/>
                     <div className="form-footer">
                         <span>Para enviar uma pergunta, <button>Faça seu login</button>.</span>
-                        <Button type="submit">Enviar pergunta</Button>
+                        <Button type="submit" disabled={!user} >Enviar pergunta</Button>
                     </div>
                 </form>
             </div>
